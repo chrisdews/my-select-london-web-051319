0bc2e5d291f704a7b8b2c49f835f10bf0e4f8e15
github username <github email address>
2019-04-20 18:49:39 +0200

    my_select working as intended

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+ 	i = 0
+ 	arr = []
+
+ 	while i < collection.length
+ 		if yield(collection[i]) == true
+			arr << collection[i]
+			i += 1
+		else
+			i += 1
+ 		end
+
+ 	end
+ 	arr
 end
